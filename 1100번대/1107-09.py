daf092243a1ec74d14d4169a8f6e61310c251ae1
S510U <fx0605@naver.com>
2024-12-07 16:33:05 +0900

    // 1day

---
+++ 1100번대/1107-09.py
@@ -0,0 +1,17 @@
+# 1107 반복 출력하기 1
+# for i in range(50):
+#     print("print",end='')
+
+# 1108 반복 출력하기 2
+# for i in range(20):
+#     print("hello",end='')
+# print(" ",end='')
+# for i in range(30):
+#     print("world",end='')
+
+# 1109 보안카드 접수증
+name=input()
+age=input()
+code=input()
+key=input()
+print(name+"\n"+age+"\n"+code+"\n"+key)
