c7f90b47be3ae881e296ab160c1b29884d0995ed
S510U <fx0605@naver.com>
2024-12-14 15:39:31 +0900

    // 5day

--- 1200번대/129n.py
+++ 1200번대/129n.py
@@ -12,3 +12,29 @@
 #     if a%i==0 and b%i==0 and c%i==0:
 #         print(i)
 #         break
+
+# 1292 범인을 잡아라 1
+# a = int(input())
+# n,sum=10,0
+#
+# for i in range(10):
+#     sum +=a%10
+#     a //= 10
+#
+# if sum%7==4: print("suspect")
+# else: print("citizen")
+
+# 1293 1등과 꼴등
+# n=int(input())
+# a = list(map(int,input().split()))
+#
+# max = a[0]
+# min = a[0]
+#
+# for i in range(1,n):
+#     if max < a[i]:
+#         max = a[i]
+#     if min>a[i]:
+#         min = a[i]
+#
+# print(max, min)
