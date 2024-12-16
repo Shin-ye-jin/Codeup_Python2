db7149a0613c8049d9e486b171af725f811505a9
S510U <fx0605@naver.com>
2024-12-16 21:02:15 +0900

    6day

---
+++ 1300번대/1359-60.py
@@ -0,0 +1,15 @@
+# 1359 숫자 피라미드 1
+# n=int(input())
+#
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j,end=' ')
+#     print()
+
+# 1360 숫자 피라미드 2
+# n=int(input())
+#
+# for i in range(n,0,-1):
+#     for j in range(i):
+#         print(i,end=' ')
+#     print()
