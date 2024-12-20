8a7dacdaa2fca7cfdb5f3668c5b1fe0715125ca7
S510U <fx0605@naver.com>
2024-12-20 21:08:26 +0900

    -6day

--- 1300번대/1359-60,62.py
+++ 1300번대/1359-60,62.py
@@ -13,3 +13,19 @@
 #     for j in range(i):
 #         print(i,end=' ')
 #     print()
+
+# 1362 숫자 피라미드 3
+def r(n):
+    if n==1:
+        return 1
+    else:
+        return n+r(n-1)
+
+n=int(input())
+re=r(n)
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(re,end=' ')
+        re-=1
+    print()
